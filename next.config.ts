a0cc7de7d3976990ec7e342f7becc96764d8b532
Inosthful <inosthful@gmail.com>
2025-06-19 19:11:58 +0200

    déploiement github actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath:
+    process.env.NODE_ENV === "production" ? "/lol-worlds-champions" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/lol-worlds-champions/" : "",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
